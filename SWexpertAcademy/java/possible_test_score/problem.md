e619733ab7c0cba0ffb6a30f45c524adaf8e63b1
doorBW <doorbw@outlook.com>
2018-10-09 18:55:31 +0900

    swexpert_possible_test_score

---
+++ SWexpertAcademy/java/possible_test_score/problem.md
@@ -0,0 +1,5 @@
+# 가능한 시험 점수
+
+해당 문제는 SW Expert Academy에서 제공하는 문제로써, 아래 링크에서 풀어보실 수 있습니다.
+
+https://swexpertacademy.com/main/solvingProblem/solvingProblem.do
---
+++ SWexpertAcademy/java/possible_test_score/solution.java
@@ -0,0 +1,74 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+    public static int answer, n, sum;
+    public static int[] checked, score;
+    public static void cal(int d){
+            if(d >= n){
+                for(int check : checked){
+                    if(check == 1) answer += 1;
+                }
+                return;
+            }
+            for(int i=sum-1;i>=0;i--){
+                if(checked[i] == 1){
+                    checked[i+score[d]] = 1;
+                }
+            }
+            cal(d+1);
+        }
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			answer = 0;
+            sum = 0;
+			n = sc.nextInt();
+            score = new int[n];
+            for(int i=0; i<n; i++){
+                score[i] = sc.nextInt();
+                sum += score[i];
+            }
+            checked = new int[sum+1];
+            checked[0] = 1;
+            cal(0);
+            System.out.println("#"+test_case+" "+answer);
+		}
+	}
+}
