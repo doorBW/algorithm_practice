7cc676f0d7adb61bd99685a7f05da8288a77ce83
doorBW <doorbw@outlook.com>
2019-05-21 22:22:18 +0900

    problem solved

--- hacker_rank/python/035.Frequency_Queries/solution.py
+++ hacker_rank/python/035.Frequency_Queries/solution.py
@@ -5,30 +5,50 @@ import os
 import random
 import re
 import sys
+from collections import Counter
 
 # Complete the freqQuery function below.
 def freqQuery(queries):
     answer = ''
-
-    dic = {}
+    f = Counter()
+    c = Counter()
     for query in queries:
         if query[0] == 1:
-            if query[1] in dic.keys():
-                dic[query[1]] += 1
-
-            else:
-                dic[query[1]] = 1
-
+            c[f[query[1]]] -= 1
+            f[query[1]] += 1
+            c[f[query[1]]] += 1
         elif query[0] == 2:
-            if (query[1] in dic.keys()) and dic[query[1]] > 0:
-                dic[query[1]] -= 1
+            if f[query[1]] > 0:
+                c[f[query[1]]] -= 1
+                f[query[1]] -= 1
+                c[f[query[1]]] += 1
         else:
-            if query[1] in dic.values():
+            if c[query[1]] > 0:
                 answer += '1'
             else:
                 answer += '0'
-
     return answer
+    # answer = ''
+    # 
+    # dic = {}
+    # for query in queries:
+    #     if query[0] == 1:
+    #         if query[1] in dic.keys():
+    #             dic[query[1]] += 1
+
+    #         else:
+    #             dic[query[1]] = 1
+
+    #     elif query[0] == 2:
+    #         if (query[1] in dic.keys()) and dic[query[1]] > 0:
+    #             dic[query[1]] -= 1
+    #     else:
+    #         if query[1] in dic.values():
+    #             answer += '1'
+    #         else:
+    #             answer += '0'
+
+    # return answer
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
